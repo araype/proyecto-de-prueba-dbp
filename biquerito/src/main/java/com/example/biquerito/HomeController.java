6b31d1be7f2c3941ee9b494e7527dc713b05f613
diego510 <66854350+diego510@users.noreply.github.com>
2023-10-26 14:56:17 -0500

    Add files via upload

---
+++ biquerito/src/main/java/com/example/biquerito/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.biquerito;
+
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomepage() {
+        return "inicio";
+    }
+
+//    @GetMapping("/registro")
+//    public String registro() {
+//        return "Registro";
+//    }
+//
+//    @GetMapping("/home")
+//    public String home() {
+//        return "home";
+//    }
+}
